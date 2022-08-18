b659d9085e160bf41533442f45c3c9f2fe4dc30d
Anuk Silva <vwv9667@autuni.ac.nz>
2022-08-18 20:25:36 +1200

    Deploying to Heroku v5

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 // app.js
-require("dotenv").config({ path: "./env" });
-const express = require("express");
-const connectDB = require("./config/db");
-var cors = require("cors");
-const books = require("./routes/api/books");
+require("dotenv").config({ path: "./env"});
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+const books = require('./routes/api/books');
 const app = express();
-const path = require("path");
-const { response } = require("express");
+const path = require('path');
+const { response } = require('express');
 
 // Connect Database
 connectDB();
 
-// cors
 app.use(cors({ origin: true, credentials: true }));
-// Init Middleware
 app.use(express.json({ extended: false }));
-
 // app.get("/", (req, res) => res.send("Hello world!"));
-
-// use Routes
-app.use("/api/books", books);
+app.use('/api/books', books);
 
 app.use(express.static(path.join(__dirname, "./my-cise-mern-book-app/build")))
 app.get("*", (req, res) => {
