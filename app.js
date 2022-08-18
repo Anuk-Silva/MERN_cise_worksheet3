c0701973ef2275dd57b96e71d93c20816903c312
Anuk Silva <vwv9667@autuni.ac.nz>
2022-08-18 22:24:29 +1200

    Deploying to Heroku

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-require("dotenv").config({ path: "./env"});
+require('dotenv').config({ path: "./env"});
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 const books = require('./routes/api/books');
 const app = express();
-const path = require('path');
+const path =  require('path');
 const { response } = require('express');
 
 connectDB();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ extended: false }));
-// app.get("/", (req, res) => res.send("Hello world!"));
 app.use('/api/books', books);
 
-app.use(express.static(path.join(__dirname, "./my-cise-mern-book-app/build")))
+app.use(express.static(path.join(dirname, "./my-cise-mern-book-app/build")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "my-cise-mern-book-app", "build", "index.html"))
+    res.sendFile(path.join(dirname, "my-cise-mern-book-app", "build", "index.html"))
 })
 
 const PORT = process.env.PORT || 5000;
