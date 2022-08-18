ea2560df2a292325c76d1cd1e40386418417f6e3
Anuk Silva <vwv9667@autuni.ac.nz>
2022-08-18 22:04:59 +1200

    changed app.js

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "my-cise-mern-book-app", "build", "index.html"))
 })
 
-//port
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
